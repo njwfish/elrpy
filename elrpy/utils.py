62b2fb3c8b09cacd40b6b52ba17e7e7c82ccc33f
njwfish <njwfish@gmail.com>
2023-06-02 21:40:21 +0100

    fix bootstrap

--- elrpy/utils.py
+++ elrpy/utils.py
@@ -9,11 +9,12 @@ def get_dims(group_data):
     return k, d, p
 
 def get_bootstrap_weights(rng, group_data, num_boots, estimate_on_all=True):
+    num_boots -= estimate_on_all
     num_groups = len(group_data[0])
     group_idx = jax.random.choice(rng, num_groups, shape=(num_groups, num_boots))
     weights = np.zeros((num_groups, num_boots))
     weights = jax.vmap(lambda w, idx:w.at[idx].add(1), in_axes=(1, 1))(weights, group_idx).T
     if estimate_on_all:
         weights = np.hstack([np.ones((num_groups, 1)), weights])
-    group_weights = {g: w for g, w in zip(group_data[0], weights)}
+    group_weights = {g: w[:, None] for g, w in zip(group_data[0], weights)}
     return group_weights
